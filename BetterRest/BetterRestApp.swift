86a33b6768dbcc22f1b498df2f597f1966e2014b
tienbui <tienbui1508@gmail.com>
2023-05-21 23:47:10 +0930

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Tien Bui on 21/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
